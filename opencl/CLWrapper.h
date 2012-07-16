daa7081fef0a5d8f6d81df6a756006e33dc09e33
Benjamin Wild <benwild@gmx.de>
2012-07-16 13:09:11 +0200

    Wrapper for OpenCL

---
+++ opencl/CLWrapper.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#define __CL_ENABLE_EXCEPTIONS
+
+#include "CL/cl.hpp"
+
+#include <vector>
+
+class CLWrapper {
+public:
+
+    CLWrapper();
+    ~CLWrapper();
+
+    cl::Program loadProgram(std::string path);
+
+    // //setup the data for the kernel
+    // //these are implemented in part1.cpp (in the future we will make these
+    // more general)
+    // void popCorn();
+    // //execute the kernel
+    // void runKernel();
+
+private:
+
+    // device variables
+    unsigned int deviceUsed;
+    std::vector<cl::Device> devices;
+
+    cl::Context context;
+
+    cl::CommandQueue queue;
+    cl::Program program;
+    cl::Kernel  kernel;
+
+    // debugging variables
+    cl_int err;
+
+    // /cl_event event;
+    cl::Event event;
+};
