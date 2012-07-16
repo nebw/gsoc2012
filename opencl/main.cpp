3d09f60da32bf8cfc3c7a8a18e16d2988c3b69bf
Benjamin Wild <benwild@gmx.de>
2012-07-16 13:10:37 +0200

    main program

---
+++ opencl/main.cpp
@@ -0,0 +1,16 @@
+#include "util.h"
+#include "CLWrapper.h"
+
+#include "CL/cl.hpp"
+
+#include <boost/program_options.hpp>
+#include <boost/foreach.hpp>
+
+int main(int argc, char **argv)
+{
+    CLWrapper test;
+
+    // example.loadProgram(kernel_source);
+
+    exit(0);
+}
