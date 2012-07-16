34a2e6e0c40accd9974ba1bf019e46610c6b5b90
Benjamin Wild <benwild@gmx.de>
2012-07-16 13:08:24 +0200

    add precompiled headers

---
+++ opencl/stdafx.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define __CL_ENABLE_EXCEPTIONS
+
+#include "CL/cl.h"
+#include "CL/cl.hpp"
+
+#include "OpenCL_FFT\clFFT.h"
+#include "OpenCL_FFT\fft_base_kernels.h"
+#include "OpenCL_FFT\fft_internal.h"
