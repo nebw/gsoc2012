f0482c9eda4a6c031cd4c25c2cfa77c700faf9b4
Benjamin Wild <benwild@gmx.de>
2012-07-21 01:32:11 +0200

    move linspaceVec to util.h

--- opencl/util.h
+++ opencl/util.h
@@ -6,3 +6,14 @@ char* file_contents(const char *filename,
                     int *length);
 
 const char* oclErrorString(cl_int error);
+
+template <typename T>
+std::vector<T> linSpaceVec(T a, T b, size_t N) {
+  T h = (b - a) / static_cast<T>(N-1);
+  std::vector<T> xs(N);
+  typename std::vector<T>::iterator x;
+  T val;
+  for (x = xs.begin(), val = a; x != xs.end(); ++x, val += h)
+    *x = val;
+  return xs;
+}
